ad23c891de1b319e72ab9b910eb6d49c12047090
offcn-java <myqq@qq.com>
2020-11-09 18:26:50 +0800

    呜呜呜呜呜呜呜呜呜呜

---
+++ src/main/java/com/offcn/FirstThymeleafController.java
@@ -0,0 +1,22 @@
+package com.offcn;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstThymeleafController {
+
+    /**
+     * 访问http://localhost:8080/first
+     * 将数据message填充到templates/index.html
+     * @param model
+     * @return
+     */
+    @GetMapping("/first")
+    public String indexPage(Model model) {
+        String message = "Hello, Thymeleaf!";
+        model.addAttribute("message", message);
+        return "index";
+    }
+}
